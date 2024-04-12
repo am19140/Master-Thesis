7d6ed6b94de84819eb5a882062dc4ac476ecc357
Mariana Petropoulou <marianapetropoulou@gmail.com>
2024-04-12 20:25:50 +0200

    removed a library

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Character from './components/Character';
+
+function App() {
+  return (
+    <>
+      <h3>Characters</h3>
+      <Character name="Warmer Type" description="You are always warm in every scenario"/>
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/components/Character.js
@@ -0,0 +1,26 @@
+function Character(props){
+    return(
+        <div className="App">
+            <div className="py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
+                <img className="block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0" src="/img/erin-lindford.jpg" alt={`${props.name}'s image`}/>
+                <div className="text-center space-y-2 sm:text-left">
+                    <div className="space-y-0.5">
+                    <p className="text-lg text-black font-semibold">
+                        {props.name}
+                    </p>
+                    <p className="text-slate-500 font-medium">
+                        {props.description}
+                    </p>
+                    </div>
+                    <button className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">Message</button>
+                </div>
+            </div>
+        </div>
+
+
+
+
+    );
+}
+
+export default Character;
