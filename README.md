e5238f183092730a34b1ab1bb978b83fad62cc56
Mariana Petropoulou <marianapetropoulou@gmail.com>
2024-06-18 11:22:58 +0200

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-# Master-Thesis
-Developing and Evaluating a User-Interactive Dashboard for real-time Thermal Comfort Feedback in LAB42, one of the buildings of University of Amsterdam
--- server/routes/swaggerAPIRouter.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-const router = express.Router();
-import {getRoomsPerFloor, getRoomTemp} from '../controllers/roomSelectionController.js';
-
-router.get('/selection', getRoomsPerFloor);
-router.get('/room_temp/:roomId',getRoomTemp);
-
-export default router;
